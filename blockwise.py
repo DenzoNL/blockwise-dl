0899b1255d8bae5abca6dc311fad4d5ff5c6134c
Denzo <denzonl@gmail.com>
2018-04-09 10:51:55 +0200

    % Refactored downloader class. + Added config option to set downloads folder

--- blockwise.py
+++ blockwise.py
@@ -2,88 +2,78 @@
 
 import json
 import mechanicalsoup
+import os
 import re
 
 
 class downloader:
-    """Object containing the Browser and the methods to login into Blockwise and Download the website"""
+    """Object containing the Browser and the methods to login into Blockwise and download the website given website"""
     browser = mechanicalsoup.StatefulBrowser()
-
     config = None
-    website_id = None
 
     def __init__(self):
-        """Constructor. Loads config.json"""
+        """Constructor. Loads the config file and logs in the user."""
+        self.load_config()
+        self.login()
 
-        # Load config.json
-        with open('config.json', 'r') as f:
-            self.config = json.load(f)
-
-    def login(self):
-        """Login the user on the Blockwise site."""
-
-        # Open browser with the login URL
-        print("Connecting to: " + self.config["login_url"])
-        self.browser.open(self.config["login_url"])
-
-        # Select the login form
-        self.browser.select_form('form[action="/login/"]')
-
-        # Fill the login form.
-        print("Attempting to login with email address: " +
-              self.config["email"])
-        self.browser["email"] = self.config["email"]
-        self.browser["password"] = self.config["password"]
-
-        # Submit form
-        self.browser.submit_selected()
-
-    def open_website_page(self):
-        """Opens the website page for the given id"""
-
-        # Construct Website url
-        website_url = "https://start.blockwi.se/websites/" + \
-            str(self.website_id) + "/pages/"
-
-        # Open Website page
-        print("Attempting to open website page with id: " + str(self.website_id))
-        self.browser.open(website_url)
-
-    def download_website(self):
+    def download(self, website_id):
         """Downloads the website zip for the given id."""
 
         print("Attempting to download website with id: " +
-              str(self.website_id))
+              str(website_id))
 
         # Construct download URL
-        download_url = "https://start.blockwi.se/websites/" + \
-            str(self.website_id) + "/pages/?action=download-confirm"
+        download_url = self.config["base_url"] + "websites/" + \
+            str(website_id) + "/pages/?action=download-confirm"
 
         # Open download page / popup
         self.browser.open(download_url)
 
         # Select download form
         self.browser.select_form(
-            'form[action="/websites/' + str(self.website_id) + '/pages/?action=download"]')
+            'form[action="/websites/' + str(website_id) + '/pages/?action=download"]')
 
         # Submit form and download contents
         response = self.browser.submit_selected()
+        self.save_file(response)
 
+    def list_websites(self):
+        """Returns a list of websites associated with this account"""
+        return
+
+    def load_config(self):
+        """Loads config.json from the same directory as this script."""
+        with open('config.json', 'r') as f:
+            self.config = json.load(f)
+
+    def login(self):
+        """Login the user onto the Blockwise site."""
+
+        # Open browser with the login URL
+        self.browser.open(self.config["base_url"] + "login")
+
+        # Select the login form
+        self.browser.select_form('form[action="/login/"]')
+
+        # Fill the login form.
+        self.browser["email"] = self.config["email"]
+        self.browser["password"] = self.config["password"]
+
+        # Submit form
+        self.browser.submit_selected()
+
+    def save_file(self, response):
+        """Saves the .zip file in the HTTP response locally"""
         # Extract filename from response url
         filename = re.search('[^/]+(?=/$|$)', response.url).group(0)
+
+        # Prepend download folder name to the filename
+        filename = self.config["folder"] + filename
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
 
         # Write contents to file
         with open(filename, 'wb') as f:
             f.write(response.content)
 
-        print("Downloaded " + filename)
-
-    def download(self, website_id):
-
-        # Set website id
-        self.website_id = website_id
-
-        # Login, open website page and download website zip
-        self.login()
-        self.open_website_page()
-        self.download_website()
+        # Print message displaying the absolute filepath for convenience
+        print("Downloaded file to " + os.path.abspath(filename))
